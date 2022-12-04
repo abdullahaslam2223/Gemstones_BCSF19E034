5238460c6bfa0b3bbd2aeb8e4efdcbe2b286d423
Muhammad Abdullah <96911855+abdullahaslam2223@users.noreply.github.com>
2022-12-04 19:55:03 +0500

    adding admin panel side menu and navbar

--- Gemstones_BCSF19E034/GS_Main.Master.designer.cs
+++ Gemstones_BCSF19E034/GS_Main.Master.designer.cs
@@ -24,15 +24,6 @@ namespace Gemstones_BCSF19E034
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// User_Email_Info control.
         /// </summary>
         /// <remarks>
@@ -40,6 +31,15 @@ namespace Gemstones_BCSF19E034
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl User_Email_Info;
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
---
+++ Gemstones_BCSF19E034/GS_ManageAdmins.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GS_Main.Master" AutoEventWireup="true" CodeBehind="GS_ManageAdmins.aspx.cs" Inherits="Gemstones_BCSF19E034.GS_ManageAdmins" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
