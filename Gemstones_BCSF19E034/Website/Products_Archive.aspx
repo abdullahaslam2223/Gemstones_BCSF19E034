e7c7233284e86b43fc7e5a71a2080799218ea9d6
Muhammad Abdullah <96911855+abdullahaslam2223@users.noreply.github.com>
2023-01-14 00:23:26 +0500

    products archive inprogress

---
+++ Gemstones_BCSF19E034/Website/Products_Archive.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Website/Website.Master" AutoEventWireup="true" CodeBehind="Products_Archive.aspx.cs" Inherits="Gemstones_BCSF19E034.Website.Products_Archive" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="Website_Head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Website_Content" runat="server">
+    <div class="row">
+        <div class="col-2">
+            <h3 id="No_Categories" runat="server"></h3>
+            <asp:GridView runat="server" CssClass="table table-bordered" AutoGenerateColumns="false" ID="GV_Product_Archive_Categories">
+                <Columns>
+                    <asp:TemplateField HeaderText="Categories">
+                        <ItemTemplate>
+                            <asp:HyperLink runat="server" NavigateUrl='<%# "~/Website/Products_Archive.aspx?id=" + Eval("category_id") %> ' Text='<%# Eval("category_name") %>'></asp:HyperLink>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+        <div class="col-10">
+            <h3 id="No_Products" runat="server"></h3>
+            <asp:GridView runat="server" ID="GV_Products_Archive_Data" CssClass="table table-bordered"></asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ Gemstones_BCSF19E034/Website/Products_Archive.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gemstones_BCSF19E034.Website
+{
+    public partial class Products_Archive : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            Fill_Categories();
+            Fill_Products();
+        }
+        private void Fill_Categories()
+        {
+            using (Gemstones_BCSF19E034Entities db = new Gemstones_BCSF19E034Entities())
+            {
+                var result = (from c in db.tbl_stone_categories select c).ToList();
+                if (result.Count > 0)
+                {
+                    GV_Product_Archive_Categories.DataSource = result;
+                    GV_Product_Archive_Categories.DataBind();
+                }
+                else
+                {
+                    No_Categories.InnerText = "No Category";
+                }
+            }
+        }
+        private void Fill_Products()
+        {
+            using (Gemstones_BCSF19E034Entities db = new Gemstones_BCSF19E034Entities())
+            {
+                var result = db.GS_Get_Products().ToList();
+                if(result.Count > 0)
+                {
+                    GV_Products_Archive_Data.DataSource = result;
+                    GV_Products_Archive_Data.DataBind();
+                } else
+                {
+                    No_Products.InnerText = "No products found";
+                }
+            }
+        }
+    }
+}
